899367a3365f10d3e43eb590421637c5005df127
Brian Page <bripage@users.noreply.github.com>
2021-11-05 09:37:22 -0400

    Update passelpp.c

--- src/passelpp.c
+++ src/passelpp.c
@@ -66,7 +66,7 @@ int main(int argc, char **argv) {
 
         printf("epoch %ld done\n", epoch);
         fflush(stdout);
-
+/*
         if (cluster_count > 1){
             train_accuracy = get_trainData_accuracy(0);
             test_accuracy = get_testData_accuracy(0);
@@ -74,7 +74,7 @@ int main(int argc, char **argv) {
             train_accuracy = get_single_trainData_accuracy(0);
             test_accuracy = get_single_testData_accuracy(0);
         }
-
+*/
         printf("%ld,%ld,%lf,%lf,%lf\n", test_id, epoch, train_accuracy, test_accuracy, epoch_time);
         fflush(stdout);
     }
