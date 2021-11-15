43a3780ca34d721d025ecf5a3cd57878ddfa170f
Brian Page <bripage@users.noreply.github.com>
2021-11-15 10:22:46 -0500

    Update sgd.c

--- src/sgd.c
+++ src/sgd.c
@@ -44,14 +44,14 @@ void update_clusters(long updater_id, long updater_mig_type, long beta_gamma) {
             //fflush(stdout);
             //for (long i = 0; i < 16; i++) {
                 cilk_migrate_hint(&model_vec[n]);
-                cilk_spawn upstream_update(i, n, upstream[n], beta_gamma);
-                //cilk_migrate_hint(&working_vec[upstream[n]]);
-                //cilk_spawn downstream_update(i, upstream[n], n);
+                //cilk_spawn upstream_update(i, n, upstream[n], beta_gamma);
+                cilk_spawn upstream_update(0, n, upstream[n], beta_gamma);
             //}
             //cilk_sync;
             //for (long i = 0; i < 16; i++) {
                 cilk_migrate_hint(&working_vec[upstream[n]]);
-                cilk_spawn downstream_update(i, upstream[n], n);
+                //cilk_spawn downstream_update(i, upstream[n], n);
+                cilk_spawn downstream_update(0, upstream[n], n);
             //}
             cilk_sync;
             updater_last_node[0][updater_id] = upstream[n];
