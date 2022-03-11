c73c17d7a237186c7477c26057c3285b87a3c8f8
Brian Page <bripage@users.noreply.github.com>
2022-03-11 16:06:01 -0500

    Update init.c

--- src/init.c
+++ src/init.c
@@ -885,7 +885,7 @@ void init() {
 
     if (using_clusters) {
         //long non_zeros_per_node = ceil(1.10 * ((double) total_train_points / (double) cluster_count));
-        mw_replicated_init((long) &non_zeros_per_node, (long) ceil(1.10 * ((double) total_train_points / (double) cluster_count)));
+        mw_replicated_init(&non_zeros_per_node, (long) ceil(1.10 * ((double) total_train_points / (double) cluster_count)));
         printf("non_zeros_per_cluster = %ld\n", non_zeros_per_node);
         fflush(stdout);
 
@@ -947,7 +947,7 @@ void init() {
         mw_replicated_init((long *) &samples_since_token, (long) l1d_ptr);
     } else {
         //long non_zeros_per_node = ceil(2 * ((double) total_train_points / (double) node_count));
-        mw_replicated_init((long) &non_zeros_per_node, (long) ceil(2 * ((double) total_train_points / (double) node_count)));
+        mw_replicated_init(&non_zeros_per_node, (long) ceil(2 * ((double) total_train_points / (double) node_count)));
         printf("nonzeros_per_node = %ld\n", non_zeros_per_node);
         fflush(stdout);
 
