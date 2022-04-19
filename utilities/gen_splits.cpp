174fc8fb8128aa239bf6d26fcdaf07c5a1e60195
Brian Page <bripage@users.noreply.github.com>
2022-04-19 10:21:50 -0400

    Update gen_splits.cpp

--- utilities/gen_splits.cpp
+++ utilities/gen_splits.cpp
@@ -69,6 +69,7 @@ int main(int argc, char **argv) {
     int64_t i;
     long n;
     long assigned_node;
+    /*
     FILE *data = fopen(fileName, "rb");
     if (data == NULL) {
         printf("Failed to open training feature file.\n");
@@ -163,5 +164,6 @@ int main(int argc, char **argv) {
         }
     }
 	std::cout << "Done!" << std::endl;
+     */
 	return 0;
 }
